6fa35d603e37e3bec2c968bb996ddcf6ce896b1c
daffaazhar <daffaazharsda@gmail.com>
2024-08-21 22:17:20 +0700

    fix: change footer

--- src/components/layout/vertical/FooterContent.tsx
+++ src/components/layout/vertical/FooterContent.tsx
@@ -33,29 +33,9 @@ const FooterContent = () => {
         <span>{`❤️`}</span>
         <span className='text-textSecondary'>{` by `}</span>
         <Link href='https://pixinvent.com' target='_blank' className='text-primary uppercase'>
-          Pixinvent
+          GenBI PENS
         </Link>
       </p>
-      {!isBreakpointReached && (
-        <div className='flex items-center gap-4'>
-          <Link href='https://themeforest.net/licenses/standard' target='_blank' className='text-primary'>
-            License
-          </Link>
-          <Link href='https://themeforest.net/user/pixinvent/portfolio' target='_blank' className='text-primary'>
-            More Themes
-          </Link>
-          <Link
-            href='https://demos.pixinvent.com/vuexy-nextjs-admin-template/documentation'
-            target='_blank'
-            className='text-primary'
-          >
-            Documentation
-          </Link>
-          <Link href='https://pixinvent.ticksy.com' target='_blank' className='text-primary'>
-            Support
-          </Link>
-        </div>
-      )}
     </div>
   )
 }
